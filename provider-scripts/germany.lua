1e4bc8991b072f94823c5e0b7f656861713a170c
Tobias Dammers <tdammers@gmail.com>
2023-04-03 16:17:38 +0200

    Nicer labels for German eAIP

--- provider-scripts/germany.lua
+++ provider-scripts/germany.lua
@@ -62,6 +62,26 @@ function listFiles(path)
             local linkURL = URL.join(navigator.currentURL, link:attr('href'))
             local path = intercalate(URL.parse(linkURL).path, '/')
             local label = link:query('span[lang=en]')[1].node.textContent
+            print(label)
+            label = string.gsub(label, '^AD 2 [A-Z][A-Z][A-Z][A-Z] ', '');
+            label = string.gsub(label, 'Aerodrome Chart', 'ADC');
+            label = string.gsub(label, 'Aerodrome Ground Movement Chart', 'AGMC');
+            label = string.gsub(label, 'Aircraft Parking/Docking Chart', 'APDC');
+            label = string.gsub(label, 'Aerodrome Obstacle Chart', 'AOC');
+            label = string.gsub(label, 'Instrument Approach Chart', 'IAC');
+            label = string.gsub(label, 'Precision Approach Terrain Chart', 'PATC');
+            label = string.gsub(label, 'ICAO', '');
+            label = string.gsub(label, 'RWY', '');
+            label = string.gsub(label, 'Arrival Chart Transition to Final Approach', 'Transition');
+            label = string.gsub(label, 'Radar Vector Pattern', 'RVP');
+            label = string.gsub(label, 'Standard Departure Routes', 'SID Routes');
+            label = string.gsub(label, 'Standard Departure Chart', 'SID');
+            label = string.gsub(label, 'Standard Arrival Routes', 'STAR Routes');
+            label = string.gsub(label, 'Standard Arrival Chart', 'STAR');
+            label = string.gsub(label, 'Departure Chart', 'SID');
+            label = string.gsub(label, 'Instrument', '');
+            label = string.gsub(label, '- *-', '-');
+            label = string.gsub(label, '- *$', '');
             table.insert(result,
                 { type = "pdf"
                 ; name = label
